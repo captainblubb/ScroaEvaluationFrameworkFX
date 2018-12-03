f34bf481dd044132a487096c7986ece5699954a8
captainblubb <fabianschulz@hotmail.de>
2018-12-03 18:08:20 +0100

    Set up tests

--- src/main/updateObject/Point3d.java
+++ src/main/updateObject/Point3d.java
@@ -10,11 +10,9 @@ public class Point3d {
         public double x,y,z;
 
         public Point3d(double x, double y,double z){
-
             this.x = x;
             this.y = y;
             this.z = z;
-
         }
 
         @Override
